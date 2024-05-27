d3fc26d61cc8c5e87b119ac6338997bf7e55134d
Arsalan Mohseni <arsalandeveloper961@gmail.com>
2024-05-27 20:18:14 +0330

    Added functions

---
+++ Simplepy/main.py
@@ -0,0 +1,25 @@
+def plus(num):
+    return num + 1
+
+def equal(var1,var2):
+    if var1 == var2:
+        return True
+
+def greater(var1,var2):
+    if var1 > var2:
+        return True
+    
+def lower(var1,var2):
+    if var1 < var2:
+        return True
+    
+def greaterequal(var1,var2):
+    if var1 >= var2:
+        return True
+    
+def lowerequal(var1,var2):
+    if var1 <= var2:
+        return True
+    
+def average(list:list):
+    return sum(list) / len(list)
